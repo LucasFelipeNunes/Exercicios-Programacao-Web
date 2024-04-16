8a2dcc1709a14e7b46caf5b4928c7fd04d960498
Lucas Felipe Nunes <lucasfelipe.lfn@gmail.com>
2024-04-16 10:52:31 -0300

    feat: create view.php

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Este repositório contém exercícios feitos para a matéria de Programação Web, do 5º semestre do curso de Análise e Desenvolvimento de Sistemas, utilizando PHP como a linguagem de programação principal.
 
 Caso queira visualizar este projeto, recomendo utilizar o servidor Apache do [XAMPP](https://www.apachefriends.org/download.html), salvando meu projeto dentro da pasta `/xampp/htdocs`.
+
+Agradeço ao [Gustavo Santos](https://github.com/stringGustavo) pela ideia da estruturação deste repositório.
---
+++ view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercícios de Programação Web</title>
+</head>
+<body>
+    <header>
+        <h1>Exercícios de Programação Web</h1>
+    </header>
+    <main>
+        <p>Olá, meu nome é Lucas Felipe Nunes! Este repositório contém alguns exercícios que eu fiz na linguagem PHP para a matéria de Programação Web.</p>
+        <p>Selecione um dos exercícios abaixo para visualizá-los:</p>
+        <ul>
+            <a href="/Atividade1"><li>Atividade 1: Formas Geométricas</li></a>
+            <a href="/Atividade2"><li>Atividade 2: </li></a>
+            <a href="/Atividade3"><li>Atividade 3: Calcula Médias - MVC</li></a>
+            <a href="/Atividade4"><li>Atividade 4: Controle de Estacionamento</li></a>
+            <a href="/Atividade5"><li>Atividade 5: Consumo de API de bandas de rock</li></a>
+        </ul>
+    </main>
+</body>
+</html>
